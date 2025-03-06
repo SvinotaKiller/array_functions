f9d36917c4d3ae1fa4432ddf9151d23bae9565ec
SvinotaKiller <riritik555@gmail.com>
2025-03-06 18:25:06 +0300

    Сделана пятая функция test.

--- main.py
+++ main.py
@@ -37,3 +37,14 @@ def filter_not_equal(arr, vaule):
             new_arr.append(number)
 
     return new_arr
+
+def test():
+
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    print(filter_greater(arr, 5))
+    print(filter_less(arr, 5))
+    print(filter_equal(arr, 5))
+    print(filter_not_equal(arr, 5))
+
+print(test())
