e4987005f76cc8e8cfb9d9f27e4e71b5dc59ad47
SvinotaKiller <riritik555@gmail.com>
2025-03-06 18:19:15 +0300

    Сделана четвертая функция filter_not_equal.

--- main.py
+++ main.py
@@ -35,3 +35,15 @@ def filter_equal(arr, vaule):
     return new_arr
 
 print(filter_equal(arr, 5))
+
+def filter_not_equal(arr, vaule):
+
+    new_arr = []
+
+    for number in arr:
+        if number != vaule:
+            new_arr.append(number)
+
+    return new_arr
+
+print(filter_not_equal(arr, 5))
